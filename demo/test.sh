a892876bd064ad9c18119f3b1480845a437cc392
ericwudayi <r07922119@ntu.edu.tw>
2019-09-16 01:05:44 +0000

    22500

---
+++ demo/test.sh
@@ -0,0 +1,9 @@
+echo "$1"
+cp -r "../../vq-vae-3-vc/wav_out" "$1"
+#echo $2
+cd "./$1"
+mv "test_mel_mel_out_inp.wav" "inp.wav"
+mv "test_mel_mel_out_inp2.wav" "inp2.wav"
+mv "test_mel_mel_out_out_m.wav" "convert.wav"
+rm "test_mel_mel_out_out.wav"
+rm "test_mel_mel_out_out_.wav"
